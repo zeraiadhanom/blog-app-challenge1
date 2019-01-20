f5274d109228b424e9550db472c3cf42ad1053dc
zeraiadhanom <zeraiadhanom@gmail.com>
2019-01-20 10:49:34 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const bprouter = require("./blogPostsRouter");
+
+const app = express();
+
+app.use(morgan("common"));
+app.use(express.json());
+
+// you need to import `blogPostsRouter` router and route
+
+// requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
+app.use('/blog-posts', bprouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
